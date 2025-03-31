a74bcee15217033d00d7dc3ecdb56f60357b2963
chari <chari@ASUSmishiro>
2025-03-31 15:49:58 +0900

    ステップ７です

--- trainingspringb/src/main/java/com/example/demo/service/UserService.java
+++ trainingspringb/src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
@@ -12,6 +15,7 @@ import com.example.demo.repository.UserRepository;
  * ユーザー情報 Service
  */
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class UserService {
 
 	/**
@@ -29,6 +33,22 @@ public class UserService {
 		return userRepository.findAll();
 	}
 
+	/**
+	 *  ユーザー情報 新規登録
+	 * @param userRequest
+	 */
+	
+	public void create(UserRequest userRequest) {
+		Date now = new Date();
+		User user =  new User();
+		user.setName(userRequest.getName());
+		user.setAddress(userRequest.getAddress());
+		user.setPhone(userRequest.getPhone());
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+		userRepository.save(user);
+	}
+	
 	/**
 	 * ユーザー情報 主キー検索
 	 * @return 検索結果
